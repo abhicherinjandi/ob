e84e0c0a46f92aceeab605cb57e6a8752ad274bc
Abhilash CV <abhilash,cv@gmail.com>
2025-04-01 22:58:27 +0530

    First Commit in master

---
+++ src/POC.java
@@ -0,0 +1,15 @@
+public class POC {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 10;
+        float b = 20.0F;
+        if ((float)a < b) {
+            System.out.println("a is less than b");
+        }
+
+        for(int i = 10; i < 20; ++i) {
+            System.out.println(i);
+        }
+        System.out.println("First Commit in master");
+    }
+}
